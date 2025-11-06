a9113193659fd60ed6f19a9c8d93356d08efec88
vitordias1006 <vitordias09532@gmail.com>
2025-11-06 09:27:47 -0300

    CONNECTIon

--- src/main/java/br/com/fiap/dao/ConnectionFactory.java
+++ src/main/java/br/com/fiap/dao/ConnectionFactory.java
@@ -23,9 +23,9 @@ public class ConnectionFactory {
                 return connection;
             }
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            String url = "DB_URL";
-            String user = "DB_USER";
-            String password = "DB_PASSWORD";
+            String url = System.getenv("DB_URL");
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASSWORD");
 
             if (url == null || user == null || password == null) {
                 throw new RuntimeException("Variaveis de ambiente não configuradas!");
